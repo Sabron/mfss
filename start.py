36d54392854b5d32fe4525f8c63520d9048fd43e
Sabron <Sabron@yandex.ru>
2022-11-01 15:18:08 +0700

    update : 01.11.2022 15:18:08,01

--- start.py
+++ start.py
@@ -201,9 +201,11 @@ def update_eps_anchors():
 def update_ops_date():
     try:
         mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all();
-        print(mfsb_list)
+        
         for mfsb in mfsb_list:
+            
             datd_mfsb = DataMfsb.objects.filter(date=mfsb.date).filter(name=mfsb.name).first()
+            print(str(mfsb.date)+' '+str(mfsb.name))
             if datd_mfsb is None:
                 DataMfsb.objects.create(
                     date=mfsb.date,
