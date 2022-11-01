4ee30f87c5a213869967ee7783570b7b28037182
Sabron <Sabron@yandex.ru>
2022-11-01 14:47:21 +0700

    update : 01.11.2022 14:47:21,45

--- start.py
+++ start.py
@@ -200,7 +200,7 @@ def update_eps_anchors():
   
 
 def update_acs_test():
-    data_mfsb = DataMfsb.objects.filter(name='MKON_SORTIROVKA.AQED_2_CO2_Otm_18000.SN_CO2_Otm_18000').filter(check=False).order_by('date').all()
+    data_mfsb = DataMfsb.objects.filter(name='MKON_BUNKER1.AQED_15_METAN_Otm_15600.Metan_Otm_15600').filter(check=False).order_by('date').all()
     print(data_mfsb)
     for data in data_mfsb:
         print(str(data.date)+' : '+data.name)
