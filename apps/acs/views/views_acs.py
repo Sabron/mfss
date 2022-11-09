ff82419d28410953e81b638b825876f2927b31d1
Your Name <you@example.com>
2022-11-10 00:37:16 +0700

    update : 10.11.2022  0:37:16,72

--- apps/acs/views/views_acs.py
+++ apps/acs/views/views_acs.py
@@ -151,7 +151,7 @@ def sensor_ajax(request):
                         date_value=TruncSecond('date_time')).values('date_time', 'date_value', 'value', 'sensor__ratio').order_by('-date_value').distinct('date_value')[:30]
             elif param['sensor_type'] == 'min':
                 strftime = "%H:%M"
-                sensor_list = AcsIndicators.objects.filter(sensor=sensor).filter(date_time__minute=0).annotate(
+                sensor_list = AcsIndicators.objects.filter(sensor=sensor).filter(date_time__second=0).annotate(
                         date_value=TruncMinute('date_time')).values('date_time','date_value', 'value', 'sensor__ratio').order_by('-date_value').distinct('date_value')[:30]
             else:
                 strftime = "%H:%M"
