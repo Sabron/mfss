15a0ca11587f9b5c136790867ebbbf8fcfc832bc
Sabron <Sabron@yandex.ru>
2023-01-19 21:51:11 +0700

    update : 19.01.2023 21:51:11,44

--- apps/ops/models/model_mfsb_ppz.py
+++ apps/ops/models/model_mfsb_ppz.py
@@ -4,7 +4,7 @@ from django.db import models
 class MfsbPpz(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(db_column='Name', max_length=150, blank=True, null=True)
-    code = models.IntegerField(db_column='Code', blank=True, null=True)
+    code = models.IntegerField(db_column='Values', blank=True, null=True)
     date = models.DateTimeField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
     check = models.BooleanField(db_column='Check')  # Field name made lowercase.
 
