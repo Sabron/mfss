f51cd03f037e411a07d6f2a8257314ffa49cc45a
Sabron <Sabron@yandex.ru>
2023-02-27 14:15:56 +0700

    update : 27.02.2023 14:15:55,97

--- start.py
+++ start.py
@@ -381,11 +381,12 @@ def update_ops_date():
     try:
         mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all();
         print('mfsb_list = '+str(mfsb_list.count()))
-        mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all()[:12000];
+        mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all()[:13000];
         print('mfsb_list = '+str(mfsb_list.count()))
         bulk = []
         for mfsb in tqdm(mfsb_list):
-            datd_mfsb = DataMfsb.objects.filter(date=mfsb.date).filter(name=mfsb.name).first()
+            datd_mfsb = DataMfsb.objects.filter(date=mfsb.date).filter(name=mfsb.name).order_by('date').first()
+            #datd_mfsb = DataMfsb.objects.filter(date__lte=mfsb.date).filter(name=mfsb.name).order_by('date').first()
             if datd_mfsb is None:
                 DataMfsb.objects.create(
                     date=mfsb.date,
@@ -399,7 +400,7 @@ def update_ops_date():
         print('Удаляем обработанные')
         Mfsb.objects.using('mfsb').filter(check=True).delete();
         print('Смотрим на старые')
-        mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all()[:5];
+        mfsb_list = Mfsb.objects.using('mfsb').filter(check=False).order_by('date').all()[:1];
         for mfsb in mfsb_list:
             print(str(mfsb.date))
     except Exception as err:
