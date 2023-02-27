857905eda55c2ff0110409e325ca71321a6d1cf2
Sabron <Sabron@yandex.ru>
2023-02-27 16:55:42 +0700

    update : 27.02.2023 16:55:42,91

--- start.py
+++ start.py
@@ -387,9 +387,9 @@ def update_acs():# Получение данных Системы Аэрогаз
     sensor_m=[]
     for data in tqdm(data_mfsb):
         sensor_link = AcsSensor.objects.filter(tag=data.name).filter(active=True).first()
-        if sensor_link not in sensor_m:
-            sensor_m.append(sensor_link)
         if sensor_link is not None:
+            if sensor_link not in sensor_m:
+                sensor_m.append(sensor_link)
             indicator_link = AcsIndicators.objects.filter(sensor = sensor_link).filter(date_time__lte=data.date).order_by('date_time')[:1]
             if indicator_link.count() > 0 :
                 if indicator_link[0].value != data.values:
@@ -422,19 +422,29 @@ def update_dcs(): # Получение данных Контроль запыл�
     sensor_list = DcsSensor.objects.values('tag').order_by('tag').distinct()
     data_mfsb = DataMfsb.objects.filter(name__in=sensor_list).filter(check=False).order_by('date').all()[:13000]
     bulk = []
+    sensor_m=[]
     for data in tqdm(data_mfsb):
         sensor_link = DcsSensor.objects.filter(tag=data.name).filter(active=True).first()
         if sensor_link is not None:
+            if sensor_link not in sensor_m:
+                sensor_m.append(sensor_link)
             Acs_Indicators = DcsIndicators.objects.create(
                 date_time =data.date,
                 sensor = sensor_link,
                 value = data.values)
-            sensor_link.value = data.values
-            sensor_link.connect_time =data.date
-            sensor_link.save()
+            #sensor_link.value = data.values
+            #sensor_link.connect_time =data.date
+            #sensor_link.save()
             data.check = True
             #data.save()
             bulk.append(data)
+    print('Обновляем последнее значение')
+    for sensor in tqdm(sensor_m):
+        indicator_link = DcsIndicators.objects.filter(sensor = sensor).order_by('date_time')[:1]
+        if indicator_link is not None:
+            sensor_link.value = indicator_link[0].value
+            sensor_link.connect_time =indicator_link[0].date_time
+            sensor_link.save()
     DataMfsb.objects.bulk_update(bulk,['check'])
 
 
