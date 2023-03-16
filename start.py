94e904ac2f017ab73f1d348d90eb7b61e620158c
Your Name <you@example.com>
2023-03-17 00:23:16 +0700

    update : 17.03.2023  0:23:16,62

--- start.py
+++ start.py
@@ -539,22 +539,23 @@ def update_block():
                                 name = data.name)
 
                 indicator_link = BlockIndicators.objects.filter(sensor = block_sensor).filter(date_time__lte=data.date).order_by('-date_time')[:1]
-                #if indicator_link.count() > 0 :
-                #    if indicator_link[0].value != data.values:
-                #        Acs_Indicators = BlockIndicators.objects.create(
-                #            date_time =data.date,
-                #            sensor = block_sensor,
-                #            value = data.values)
-                #else:
-                #    Acs_Indicators = BlockIndicators.objects.create(
-                #        date_time =data.date,
-                #        sensor = block_sensor,
-                #        value = data.values)
-                #block_sensor.value = data.values
-                #block_sensor.connect_time =data.date
-                #block_sensor.save()
-                #data.check = True
-                #data.save()
+                if indicator_link.count() > 0 :
+                    if indicator_link[0].value != data.values:
+                        Acs_Indicators = BlockIndicators.objects.create(
+                            date_time =data.date,
+                            sensor = block_sensor,
+                            value = data.values)
+                else:
+                    Acs_Indicators = BlockIndicators.objects.create(
+                        date_time =data.date,
+                        sensor = block_sensor,
+                        value = data.values)
+                block_sensor.value = data.values
+                block_sensor.connect_time =data.date
+                block_sensor.save()
+                data.check = True
+                print('Запись')
+                data.save()
                 #bulk.append(data)
                 #if len(bulk) > 500:
                 #    MfsbBlock.objects.using('mfsb_block').bulk_update(bulk,['check'])
