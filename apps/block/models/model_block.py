a86bbac19791b131e9bdb95537419dfae87f0376
Your Name <you@example.com>
2023-03-17 23:47:04 +0700

    update : 17.03.2023 23:47:04,66

--- apps/block/models/model_block.py
+++ apps/block/models/model_block.py
@@ -5,7 +5,7 @@ class Block(models.Model):
     name = models.CharField(db_column='Name', max_length=150, blank=True, null=True)  # Field name made lowercase.
     values = models.FloatField(db_column='Values', blank=True, null=True)  # Field name made lowercase.
     date = models.DateTimeField(db_column='Date', blank=True, null=True)  # Field name made lowercase.
-    check = models.BooleanField(db_column='Check')  # Field name made lowercase.
+    check = models.BooleanField(db_column='Check', blank=True, null=True,default=False)  # Field name made lowercase.
 
     class Meta:
         verbose_name = u'Блокировки'
